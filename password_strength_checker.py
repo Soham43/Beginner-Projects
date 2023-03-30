0616588580c0d2a46ba7e37b1ff2cbe6aeeaf91c
Soham43 <111642126+Soham43@users.noreply.github.com>
2023-03-30 11:26:10 +0530

    Add files via upload

---
+++ password_strength_checker.py
@@ -0,0 +1,27 @@
+def check_password_strength(password):
+    '''
+    program to check strength of input password
+    Parameter:
+    password(str): input password
+
+    return:
+    output(str): week or average or strong
+    '''
+    special_chars  = list('@#$')
+    isdigit_there  = any(char.isdigit() for char in password)
+    isupper_there  = any(char.isupper() for char in password)
+    isspchar_there = any(char.isdigit() for char in password)
+    check_lower    = any(char.islower() for char in password)
+
+    all_true = all([isdigit_there,isupper_there, isspchar_there, check_lower])
+    if len(password)<6:
+        return "Weak"
+    elif len(password)>=8 and all_true:
+        return "Strong"
+    else:
+        return "Average"
+    
+input_password = input("Create Password: ")
+strength       = check_password_strength(input_password)
+print("")
+print("Your password is {} !!".format(strength))
